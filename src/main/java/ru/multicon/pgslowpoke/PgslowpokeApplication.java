29cbccd58a97d0f94a4b908c9d2673f60d162d37
Max Samkov <littlebiggame2@gmail.com>
2018-12-13 01:54:25 +0300

    1) Создал заготовку. 2) Добавил в docker-compose два кластера Postgres.   Первый db-analytics для хранения аналитики.   Второй db-target для создания тестовой БД с дубликатами индексов и внешних ключей.   Цель найти изъяны в БД db-target.

---
+++ src/main/java/ru/multicon/pgslowpoke/PgslowpokeApplication.java
@@ -0,0 +1,14 @@
+package ru.multicon.pgslowpoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgslowpokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgslowpokeApplication.class, args);
+	}
+
+}
+
