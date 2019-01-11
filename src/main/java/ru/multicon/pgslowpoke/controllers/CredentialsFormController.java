f3f7b58e00d7a132d06d72a9d3764992a80b6dd0
aszimka <aszimka@gmail.com>
2019-01-11 19:01:50 +0300

    Сделал форму для ввода реквезитов подключения к БД

--- src/main/java/ru/multicon/pgslowpoke/controllers/CredentialsFormController.java
+++ src/main/java/ru/multicon/pgslowpoke/controllers/CredentialsFormController.java
@@ -3,12 +3,22 @@ package ru.multicon.pgslowpoke.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import ru.multicon.pgslowpoke.domain.CredentialsDTO;
 
 @Controller
 public class CredentialsFormController {
 
     @GetMapping("/credentials")
     public String credentials(Model model) {
+        model.addAttribute("credentialsDTO", new CredentialsDTO());
         return "credentials";
+    }
+
+    @PostMapping("/inputcredentials")
+    public String inputCredentials(@ModelAttribute CredentialsDTO credentialsDTO) {
+        //TODO сохранять введенный datasource
+        return "credentialsSuccess";
     }
 }
