86eea04917458da843ff41ba9cb6b2ce2a28226a
aszimka <aszimka@gmail.com>
2019-01-11 11:37:22 +0300

    Добавил страницу ввода реквизитов

---
+++ src/main/java/ru/multicon/pgslowpoke/controllers/CredentialsFormController.java
@@ -0,0 +1,14 @@
+package ru.multicon.pgslowpoke.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CredentialsFormController {
+
+    @GetMapping("/credentials")
+    public String credentials(Model model) {
+        return "credentials";
+    }
+}
