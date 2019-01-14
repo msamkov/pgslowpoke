01d8e1fd2d963a262101af0ff91635a36ed16d65
Max Samkov <littlebiggame2@gmail.com>
2019-01-14 18:23:45 +0300

    Unit-тест для сервиса PgSettingsHintService

---
+++ src/test/java/ru/multicon/pgslowpoke/services/PgSettingsHintServiceTest.java
@@ -0,0 +1,23 @@
+package ru.multicon.pgslowpoke.services;
+
+import org.junit.Assert;
+import org.junit.Test;
+import ru.multicon.pgslowpoke.domain.PgSettingsHint;
+import java.util.List;
+
+public class PgSettingsHintServiceTest {
+
+    @Test
+    public void findAll() {
+        //подготавливаем
+        PgSettingsHintService pgSettingsHintService = new PgSettingsHintService();
+        int min = 1;
+
+        //выполняем
+        List<PgSettingsHint> pgSettingsHints = pgSettingsHintService.findAll();
+        int actual = pgSettingsHints.size();
+
+        //сравниваем
+        Assert.assertTrue(actual > min);
+    }
+}
