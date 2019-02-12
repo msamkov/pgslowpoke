ca875c1339ebd1a58ba36cf853d65d12b2c781d2
Max Samkov <littlebiggame2@gmail.com>
2019-02-12 20:47:34 +0300

    [issue #37] Поправил тест для сервиса PgSettingsService

--- src/test/java/ru/multicon/pgslowpoke/services/PgSettingsServiceTest.java
+++ src/test/java/ru/multicon/pgslowpoke/services/PgSettingsServiceTest.java
@@ -1,94 +1,106 @@
 package ru.multicon.pgslowpoke.services;
 
-import org.junit.Before;
+import com.zaxxer.hikari.HikariDataSource;
+import org.junit.Assert;
 import org.junit.Test;
-import org.mockito.Mock;
+import ru.multicon.pgslowpoke.domain.PgCredentials;
 import ru.multicon.pgslowpoke.domain.PgSettings;
 import ru.multicon.pgslowpoke.domain.PgSettingsHint;
 import ru.multicon.pgslowpoke.repositories.PgSettingsRepository;
+import ru.multicon.pgslowpoke.utils.DataSourceFactory;
+import ru.multicon.pgslowpoke.utils.MyBatisMapperFactory;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class PgSettingsServiceTest {
 
-//    @Mock
-//    private PgSettingsRepository pgSettingsRepository =
-//        mock(PgSettingsRepository.class, RETURNS_DEEP_STUBS);
-//
-//    @Mock
-//    private PgSettingsHintService pgSettingsHintService =
-//        mock(PgSettingsHintService.class, RETURNS_DEEP_STUBS);
-//
-//    private PgSettingsService pgSettingsService;
-//
-//    private final List<PgSettings> settings = Arrays.asList(
-//        new PgSettings("n1", "v1", "s1", "mn1", "mx1", "u1", null),
-//        new PgSettings("n2", "v2", "s2", "mn2", "mx2", "u2", null),
-//        new PgSettings("n3", "v3", "s3", "mn3", "mx3", "u3", null)
-//    );
-//
-//    private final List<PgSettingsHint> settingsHints = Arrays.asList(
-//        new PgSettingsHint("n1", "d1", "h1"),
-//        new PgSettingsHint("n3", null, "h3")
-//    );
-//
-//    @Before
-//    public void setUp() {
-//        pgSettingsService = new PgSettingsService(pgSettingsRepository, pgSettingsHintService);
-//    }
-//
-//    @Test
-//    public void findAllEmpty() {
-//        when(pgSettingsRepository.findAll()).thenReturn(Collections.emptyList());
-//        when(pgSettingsHintService.findAll()).thenReturn(Collections.emptyList());
-//        final List<PgSettings> result = pgSettingsService.findAll();
-//        assertEquals(Collections.emptyList(), result);
-//        verify(pgSettingsRepository, times(1)).findAll();
-//        verify(pgSettingsHintService, times(1)).findAll();
-//    }
-//
-//    @Test
-//    public void findAll() {
-//        when(pgSettingsRepository.findAll()).thenReturn(settings);
-//        when(pgSettingsHintService.findAll()).thenReturn(settingsHints);
-//        final List<PgSettings> result = pgSettingsService.findAll();
-//        final List<PgSettings> expected = Arrays.asList(
-//            new PgSettings("n1", "v1", "s1", "mn1", "mx1", "u1", "d1"),
-//            new PgSettings("n2", "v2", "s2", "mn2", "mx2", "u2", null),
-//            new PgSettings("n3", "v3", "s3", "mn3", "mx3", "u3", null)
-//        );
-//        assertEquals(expected, result);
-//        verify(pgSettingsRepository, times(1)).findAll();
-//        verify(pgSettingsHintService, times(1)).findAll();
-//    }
-//
-//    @Test
-//    public void findPrimarySettingsEmpty() {
-//        when(pgSettingsRepository.findPrimarySettings()).thenReturn(Collections.emptyList());
-//        when(pgSettingsHintService.findAll()).thenReturn(Collections.emptyList());
-//        final List<PgSettings> result = pgSettingsService.findPrimarySettings();
-//        assertEquals(Collections.emptyList(), result);
-//        verify(pgSettingsRepository, times(1)).findPrimarySettings();
-//        verify(pgSettingsHintService, times(1)).findAll();
-//    }
-//
-//    @Test
-//    public void findPrimarySettings() {
-//        when(pgSettingsRepository.findPrimarySettings()).thenReturn(settings);
-//        when(pgSettingsHintService.findAll()).thenReturn(settingsHints);
-//        final List<PgSettings> result = pgSettingsService.findPrimarySettings();
-//        final List<PgSettings> expected = Arrays.asList(
-//            new PgSettings("n1", "v1", "s1", "mn1", "mx1", "u1", "d1"),
-//            new PgSettings("n2", "v2", "s2", "mn2", "mx2", "u2", null),
-//            new PgSettings("n3", "v3", "s3", "mn3", "mx3", "u3", null)
-//        );
-//        assertEquals(expected, result);
-//        verify(pgSettingsRepository, times(1)).findPrimarySettings();
-//        verify(pgSettingsHintService, times(1)).findAll();
-//    }
+    @Test
+    public void findAll() {
+        //подготавливаем
+        PgSettingsHintService pgSettingsHintService = mock(PgSettingsHintService.class);
+        MyBatisMapperFactory myBatisMapperFactory = mock(MyBatisMapperFactory.class);
+        PgSettingsRepository pgSettingsRepository = mock(PgSettingsRepository.class);
+        PgCredentials pgCredentials = mock(PgCredentials.class);
+        DataSourceFactory dataSourceFactory = mock(DataSourceFactory.class);
+        PgSettingsService pgSettingsService =
+                new PgSettingsService(myBatisMapperFactory, pgSettingsHintService, dataSourceFactory);
+        HikariDataSource dataSource = mock(HikariDataSource.class);
+        final List<PgSettings> pgSettings = Arrays.asList(
+                new PgSettings("n1", "v1", "s1", "mn1", "mx1", "u1", null),
+                new PgSettings("n2", "v2", "s2", "mn2", "mx2", "u2", null),
+                new PgSettings("n3", "v3", "s3", "mn3", "mx3", "u3", null));
+
+        String desc1 = "desc 1";
+        String desc2 = "desc 2";
+        String desc3 = "desc 3";
+        final List<PgSettings> expected = Arrays.asList(
+                new PgSettings("n1", "v1", "s1", "mn1", "mx1", "u1", desc1),
+                new PgSettings("n2", "v2", "s2", "mn2", "mx2", "u2", desc2),
+                new PgSettings("n3", "v3", "s3", "mn3", "mx3", "u3", desc3));
+
+
+        final List<PgSettingsHint> pgSettingsHints = Arrays.asList(
+                new PgSettingsHint("n1", desc1, null),
+                new PgSettingsHint("n2", desc2, null),
+                new PgSettingsHint("n3", desc3, null));
+
+        when(pgSettingsService.getHikariDataSource(pgCredentials)).thenReturn(dataSource);
+        when(pgSettingsService.getPgSettingsRepository(dataSource)).thenReturn(pgSettingsRepository);
+        when(pgSettingsRepository.findAll()).thenReturn(pgSettings);
+        when(pgSettingsHintService.findAll()).thenReturn(pgSettingsHints);
+
+        //выполняем
+        List<PgSettings> actual = pgSettingsService.findAll(pgCredentials);
+
+        //сравниваем
+        Assert.assertEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void findPrimarySettings() {
+        //подготавливаем
+        PgSettingsHintService pgSettingsHintService = mock(PgSettingsHintService.class);
+        MyBatisMapperFactory myBatisMapperFactory = mock(MyBatisMapperFactory.class);
+        PgSettingsRepository pgSettingsRepository = mock(PgSettingsRepository.class);
+        PgCredentials pgCredentials = mock(PgCredentials.class);
+        DataSourceFactory dataSourceFactory = mock(DataSourceFactory.class);
+        PgSettingsService pgSettingsService =
+                new PgSettingsService(myBatisMapperFactory, pgSettingsHintService, dataSourceFactory);
+        HikariDataSource dataSource = mock(HikariDataSource.class);
+        final List<PgSettings> pgSettings = Arrays.asList(
+                new PgSettings("n1", "v1", "s1", "mn1", "mx1", "u1", null),
+                new PgSettings("n2", "v2", "s2", "mn2", "mx2", "u2", null),
+                new PgSettings("n3", "v3", "s3", "mn3", "mx3", "u3", null));
+
+        String desc1 = "desc 1";
+        String desc2 = "desc 2";
+        String desc3 = "desc 3";
+        final List<PgSettings> expected = Arrays.asList(
+                new PgSettings("n1", "v1", "s1", "mn1", "mx1", "u1", desc1),
+                new PgSettings("n2", "v2", "s2", "mn2", "mx2", "u2", desc2),
+                new PgSettings("n3", "v3", "s3", "mn3", "mx3", "u3", desc3));
+
+
+        final List<PgSettingsHint> pgSettingsHints = Arrays.asList(
+                new PgSettingsHint("n1", desc1, null),
+                new PgSettingsHint("n2", desc2, null),
+                new PgSettingsHint("n3", desc3, null));
+
+        when(pgSettingsService.getHikariDataSource(pgCredentials)).thenReturn(dataSource);
+        when(pgSettingsService.getPgSettingsRepository(dataSource)).thenReturn(pgSettingsRepository);
+        when(pgSettingsRepository.findPrimarySettings()).thenReturn(pgSettings);
+        when(pgSettingsHintService.findAll()).thenReturn(pgSettingsHints);
+
+        //выполняем
+        List<PgSettings> actual = pgSettingsService.findPrimarySettings(pgCredentials);
+
+        //сравниваем
+        Assert.assertEquals(expected, actual);
+
+    }
 }
