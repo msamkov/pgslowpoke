06e1479c7c675342788629be88c3839ffcde541e
msamkov <littlebiggame2@gmail.com>
2019-11-18 19:03:36 +0300

    Подготовка для Heroku. Ограничение тестовых данных с 40k до 4k т.к. в Heroku ограничение у Хобби аккаунты 10k.

--- src/main/resources/db/v-2.0.0/sql/test_for_index_unused.sql
+++ src/main/resources/db/v-2.0.0/sql/test_for_index_unused.sql
@@ -1,11 +1,11 @@
--- Создаём 40000 тестовых строк
+-- Создаём 4000 тестовых строк
 INSERT INTO s_test.t_index_unused(id, name, title, enabled, size)
 SELECT g AS id,
        md5(random()::text) as name,
        md5(random()::text) as title,
        random()::int::boolean AS enabled,
        random()*1000 as size
-FROM generate_series(1,10000) AS g;
+FROM generate_series(1,1000) AS g;
 ANALYZE s_test.t_index_unused;
 
 INSERT INTO s_test.t_index_unused(id, name, title, enabled, size)
@@ -14,7 +14,7 @@ SELECT g AS id,
        md5(random()::text) as title,
        random()::int::boolean AS enabled,
        random()*1000 as size
-FROM generate_series(10001,20000) AS g;
+FROM generate_series(1001,2000) AS g;
 ANALYZE s_test.t_index_unused;
 
 INSERT INTO s_test.t_index_unused(id, name, title, enabled, size)
@@ -23,7 +23,7 @@ SELECT g AS id,
        md5(random()::text) as title,
        random()::int::boolean AS enabled,
        random()*1000 as size
-FROM generate_series(20001,30000) AS g;
+FROM generate_series(2001,3000) AS g;
 ANALYZE s_test.t_index_unused;
 
 INSERT INTO s_test.t_index_unused(id, name, title, enabled, size)
@@ -32,7 +32,7 @@ SELECT g AS id,
        md5(random()::text) as title,
        random()::int::boolean AS enabled,
        random()*1000 as size
-FROM generate_series(30001,40000) AS g;
+FROM generate_series(3001,4000) AS g;
 ANALYZE s_test.t_index_unused;
 
 -- Делаем 300 полных проходов по таблице
